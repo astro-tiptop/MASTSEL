6f5a77c8f813694335a16c7f886bf94688a6ca1e
Guido Agapito <guido.agapito@inaf.it>
2024-06-18 15:07:27 +0200

    (GA) noise variance bugs

--- mastsel/mavisLO.py
+++ mastsel/mavisLO.py
@@ -1223,19 +1223,23 @@ class MavisLO(object):
             
         for starIndex in range(nNaturalGS):
             self.configLOFreq( aNGS_freq[starIndex] )
+            # one scalar (bias), two tuples of 2 (amu, avar)
             if self.simpleVarianceComputation:
-                bias, amu, avar = self.simplifiedComputeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE_LO[starIndex], aNGS_FWHM_mas[starIndex]) # one scalar, two
+                bias, amu, avar = self.simplifiedComputeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE_LO[starIndex], aNGS_FWHM_mas[starIndex])
+                # conversion from rad2 to mas2
+                rad2mas = (self.SensingWavelength_LO*1e9/(2*np.pi)) * (1000*4e-9/(self.TelescopeDiameter/self.NumberLenslets[0])*206264.8)
+                var1x = avar[0] * rad2mas**2
             else:
-                bias, amu, avar = self.computeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE_LO[starIndex], aNGS_FWHM_mas[starIndex]) # one scalar, two tuples of 2
-            
-            # normalized by the number of subapertures
-            avar = tuple((1.0/self.N_sa_tot_LO) * elem for elem in avar)
+                bias, amu, avar = self.computeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE_LO[starIndex], aNGS_FWHM_mas[starIndex])
+                # conversion from pixel2 to mas2
+                var1x = avar[0] * self.PixelScale_LO**2
 
             self.bias.append(bias)
             self.amu.append(amu)
             self.avar.append(avar)
 
             var1x = avar[0] * self.PixelScale_LO**2
+            # noise propagation coefficient (from mas2 to nm2) is applied in computeNoiseResidual
             nr = self.computeNoiseResidual(0.25, self.maxLOtFreq, int(4*self.maxLOtFreq), var1x, bias, self.platformlib )
             # This computation is skipped if no wind shake PSD is present.
             if np.sum(self.psd_tip_wind) > 0 or np.sum(self.psd_tilt_wind) > 0:
@@ -1272,16 +1276,22 @@ class MavisLO(object):
         if self.verbose:
             print('mavisLO.computeFocusTotalResidualMatrix')
             
-        for starIndex in range(nNaturalGS):
+        for starIndex in range(nNaturalGS):           
             self.configLOFreq( aNGS_freq[starIndex] )
+            # one scalar (bias), two tuples of 2 (amu, avar)
             if self.simpleVarianceComputation:
-                bias, amu, avar = self.simplifiedComputeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE_LO[starIndex], aNGS_FWHM_mas[starIndex]) # one scalar, two
+                bias, amu, avar = self.simplifiedComputeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE_LO[starIndex], aNGS_FWHM_mas[starIndex])
+                # conversion from rad2 to mas2
+                rad2mas = (self.SensingWavelength_LO*1e9/(2*np.pi)) * (1000*4e-9/(self.TelescopeDiameter/self.NumberLenslets[0])*206264.8)
+                var1x = avar[0] * rad2mas**2
             else:
-                bias, amu, avar = self.computeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE_LO[starIndex], aNGS_FWHM_mas[starIndex]) # one scalar, two tuples of 2
+                bias, amu, avar = self.computeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE_LO[starIndex], aNGS_FWHM_mas[starIndex])
+                # conversion from pixel2 to mas2
+                var1x = avar[0] * self.PixelScale_LO**2
             
-            # normalized by the number of subapertures
-            avar = tuple((1.0/self.N_sa_tot_LO) * elem for elem in avar) # TODO update with noise propagation for focus
-            var1x = avar[0] * self.PixelScale_LO**2
+            # noise propagation for focus is 0.4 time the tilt one (linear)
+            var1x *= 0.16
+            # noise propagation coefficient (from mas2 to nm2) is applied in computeNoiseResidual (except for 0.16 factor above)
             Cnn[starIndex,starIndex] = self.computeFocusNoiseResidual(0.25, self.maxLOtFreq, int(4*self.maxLOtFreq), var1x, bias, self.platformlib )
             
         # NGS Rec. Mat.
