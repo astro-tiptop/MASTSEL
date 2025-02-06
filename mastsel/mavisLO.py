9d27afc38c96d9cd32f7a919743ded9d9fad17b5
Guido Agapito <guido.agapito@inaf.it>
2025-02-06 10:54:57 +0100

    (GA) tilt noise propagation normalization (#55)

--- mastsel/mavisLO.py
+++ mastsel/mavisLO.py
@@ -1306,6 +1306,8 @@ class MavisLO(object):
                 bias, amu, avar = self.computeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE[starIndex], aNGS_FWHM_mas[starIndex])
                 # conversion from pixel2 to mas2
                 var1x = avar[0] * self.PixelScale_LO**2
+            # noise propagation coefficient on tip/tilt is normalized by the number of sub-apertures
+            var1x /= NumberLenslets
 
             self.bias.append(bias)
             self.amu.append(amu)
@@ -1400,13 +1402,17 @@ class MavisLO(object):
             
         for starIndex in range(nNaturalGS):
             self.configFocusFreq( aNGS_freq[starIndex] )
+            if nNaturalGS != len(self.NumberLenslets_Focus):
+                NumberLenslets = self.NumberLenslets_Focus[0]
+            else:
+                NumberLenslets = self.NumberLenslets_Focus[starIndex]
             # one scalar (bias), two tuples of 2 (amu, avar)
             if self.simpleVarianceComputation or self.check_section_key('sensor_Focus'):
                 bias, amu, avar = self.simplifiedComputeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex],
                                                                         aNGS_EE[starIndex], aNGS_FWHM_mas[starIndex], doLO=False)
                 # conversion from rad2 (peak-to-valley) to mas2
                 # rad to OPD --> wavelength / (2pi), OPD to arcsec --> 3600*180/pi / D_SA, arcsec to mas --> 1000 and factor 4 from RMS to peak-to-valley
-                rad2mas = self.SensingWavelength_Focus*2000*206264.8/(np.pi*self.TelescopeDiameter/self.NumberLenslets_Focus[0])
+                rad2mas = self.SensingWavelength_Focus*2000*206264.8/(np.pi*self.TelescopeDiameter/NumberLenslets)
                 var1x = avar[0] * rad2mas**2
             else:
                 bias, amu, avar = self.computeBiasAndVariance(aNGS_flux[starIndex], aNGS_freq[starIndex], aNGS_EE[starIndex], aNGS_FWHM_mas[starIndex])
