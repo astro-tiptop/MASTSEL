30a3f0d67728a57044b3606f753243ce8ecfbee4
guido.agapito <guido.agapito@inaf.it>
2025-03-12 17:23:18 +0100

    (GA) longExposurePsf can return cupy array, xp in psdSetToPsfSet, and sign of shifts

--- mastsel/mavisPsf.py
+++ mastsel/mavisPsf.py
@@ -409,9 +409,7 @@ def longExposurePsf(mask, psd, otf_tel = None):
 
     # step 5 : system otf to system psf
     result.sampling = xp.real(ft_ft2(otf_system))
-    if xp.__name__=='cupy':
-        result.sampling =  xp.asnumpy(result.sampling)
-        
+
     return result
 
 
@@ -468,10 +466,7 @@ def psdSetToPsfSet(inputPSDs, mask, wavelength, N, nPixPup, grid_diameter, freq_
 
     psfLongExpArr = []
 
-    if mask.xp == cp:
-        from cupyx.scipy.ndimage import shift as a_shift
-    else:
-        from scipy.ndimage import shift as a_shift
+    xp = None
 
     for wvl, ovrsmp in zip(wavelength, oversampling):
         if padPSD:
@@ -487,6 +482,13 @@ def psdSetToPsfSet(inputPSDs, mask, wavelength, N, nPixPup, grid_diameter, freq_
         if not isinstance(mask, list):
             maskField.sampling = congrid(mask, [nPixPup, nPixPup])
             maskField.sampling = zeroPad(maskField.sampling, (nPad - nPixPup) // 2)
+
+        if xp is None:
+            xp = maskField.xp
+            if xp == cp:
+                from cupyx.scipy.ndimage import shift as a_shift
+            else:
+                from scipy.ndimage import shift as a_shift
 
         if opdMap is None:
             otf_tel = None
@@ -523,7 +525,7 @@ def psdSetToPsfSet(inputPSDs, mask, wavelength, N, nPixPup, grid_diameter, freq_
             # enlarge the PSF to get a side multiple of ovrsmp
             nBig = int(np.ceil(psfLE.N/ovrsmp/2)*ovrsmp*2)
             if nBig > psfLE.N:
-                psfLE.sampling = zeroPad(cp.array(psfLE.sampling), (nBig-psfLE.N)//2)
+                psfLE.sampling = zeroPad(psfLE.sampling, (nBig-psfLE.N)//2)
             # resample the PSF to get the not oversampled pixel scale
             if ovrsmp > 1:
                 nOvr = int(ovrsmp)
@@ -533,10 +535,10 @@ def psdSetToPsfSet(inputPSDs, mask, wavelength, N, nPixPup, grid_diameter, freq_
                 delta = (ovrsmp-1)/2
                 if ovrsmp % 2:
                     # integer shifts
-                    psfLE.sampling = psfLE.xp.roll(psfLE.sampling, (-int(delta), -int(delta)), axis=(0, 1))
+                    psfLE.sampling = psfLE.xp.roll(psfLE.sampling, (int(delta), int(delta)), axis=(0, 1))
                 else:
                     # non integer shifts
-                    psfLE.sampling = a_shift(psfLE.sampling, (-delta, -delta), order=3, mode='constant'))
+                    psfLE.sampling = a_shift(psfLE.sampling, (delta, delta), order=3, mode='constant')
 
                 # rebin the PSF
                 psfLE.sampling = psfLE.sampling.reshape((nOut, nOvr, nOut, nOvr)).mean(3).mean(1)
